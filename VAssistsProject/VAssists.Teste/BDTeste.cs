ad061113a16e445fe113f3070fef6a223341bcc1
ARTHUR-PC\Arthur-Norberto <arthurcostalima@hotmail.com>
2017-05-04 20:03:21 -0300

    Correções

---
+++ VAssistsProject/VAssists.Teste/BDTeste.cs
@@ -0,0 +1,22 @@
+﻿using MySql.Data.MySqlClient;
+using NUnit.Framework;
+using System.Data;
+
+namespace VAssists.Teste
+{
+    [TestFixture]
+    public class BDTeste
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            MySqlConnection bdConn; //MySQL
+            //Define string de conexão
+            bdConn = new MySqlConnection("Server=localhost;Database=producao;Uid=root;Pwd=root;");
+
+            Assert.DoesNotThrow(() => bdConn.Open());
+
+            Assert.AreEqual(ConnectionState.Open, bdConn.State);
+        }
+    }
+}
